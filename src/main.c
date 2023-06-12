7db76acf36939559fccc01218907c98d80d0010f
Alexis LOUBIERE <alex.loubiere@gmail.com>
2023-06-13 00:39:57 +0200

    First part of the pipe working. Added a test file for the program as well.

--- src/main.c
+++ src/main.c
@@ -1,51 +1,73 @@
 #include "pipex.h"
 
-// void    child_process(int *pipe_fd, char **argv, char **path)
-// {
-//     // int fd;
+char    *get_cmd(char *cmd, char **env_p);
 
-//     // fd = open(av[1], 1);
-//     // dup2(fd, 0);
-// }
+void    do_cmd(char *cmd, char **env_p)
+{
+    char    **cmd_split;
+    char    *cmd_p;
 
-int main(int argc, char **argv, char **env_p)
+    cmd_split = ft_split(cmd, ' ');
+    cmd_p = get_cmd(cmd_split[0], env_p);
+    printf("cmd = %s\n", cmd_p);
+    if (execve(cmd_p, cmd_split, env_p) == -1)
+    {
+        ft_putstr_fd("Error", 2);
+        exit(0);
+    }
+}
+
+char    *get_cmd(char *cmd, char **env_p)
+{
+    int i;
+    char    *cmd_root;
+    char    *cmd_tmp;
+    i = -1;
+    while (env_p[++i])
+	{
+		cmd_root = ft_strjoin(env_p[i], "/");
+		cmd_tmp = ft_strjoin(cmd_root, cmd);
+		free(cmd_root);
+		if (access(cmd_tmp, F_OK | X_OK) == 0)
+		{
+            return (cmd_tmp);
+		}
+		free(cmd_tmp);
+	}
+    return (cmd);
+}
+void    child_process(int *pipe_fd, char **argv, char **env_p)
+{
+    int fd;
+
+    fd = open(argv[1], O_RDONLY, 0777);
+    dup2(fd, 0);
+    // close(pipe_fd[1]);
+    // close(pipe_fd[0]);
+    (void)pipe_fd;
+    do_cmd(argv[2], env_p);
+}
+
+int main(int argc, char **argv)
 {
     int     pipe_fd[2];
-    pid_t   pid;
-    char    **cmd;
+    // pid_t   pid;
     char    *env;
-    char    **path;
-    char    *exec;
-    char    *path_part;
-    int     i;
+    char    **env_p;
 
-    if (argc != 5)
-        (void)env_p;
+    if (argc != 3)
+        return (0);
     env = getenv("PATH");
-    path = ft_split(env, ':');
-    cmd = ft_split(argv[1], ' ');
-    i = -1;
-    while (path[++i])
-	{
-		path_part = ft_strjoin(path[i], "/");
-		exec = ft_strjoin(path_part, argv[1]);
-		free(path_part);
-        exec = ft_strtrim(exec, ">");
-        printf("%s\n", exec);
-		if (access(exec, F_OK | X_OK) == 0)
-		{
-            execve(exec, cmd, env_p);
-		}
-		free(exec);
-	}
-    if (pipe(pipe_fd) == -1)
-        return (-1);
-    pid = fork();
-    (void)argv;
-    if (pid == -1)
-        return (1);
-    if (!pid)
-    //     // child_process();
+    env_p = ft_split(env, ':');
+
+    // if (pipe(pipe_fd) == -1)
+    //     return (-1);
+    // pid = fork();
+    // (void)argv;
+    // if (pid == -1)
+    //     return (1);
+    // if (!pid)
+    child_process(pipe_fd, argv, env_p);
     // else
     //     parent_process();
     return (0);
