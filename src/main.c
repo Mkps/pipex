586a6ce04e10029cb6eea6aca5ce52f8c1d557c8
Alexis LOUBIERE <alex.loubiere@gmail.com>
2023-06-12 16:04:27 +0200

    creating structure to fork the pid.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,18 @@
-#include <stdio.h>
+#include "pipex.h"
+#include <unistd.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
-    printf("Hello world!\n");
+    int   fd[2];
+    pid_t pid;
+
+    if (argc < 5)
+        printf("Hello world!\n");
+    if (pipe(fd) == -1)
+        return (-1);
+    pid = fork();
+    (void)argv;
+    if (pid == -1)
+        return (0);
     return (0);
 }
