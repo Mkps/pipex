72af7c2e050b3f996922295a21a334a1ac649fd8
Alexis LOUBIERE <aloubier@student.42.fr>
2023-07-02 08:18:10 +0200

    norminette OK on pipex_utils.c

--- src/pipex_utils.c
+++ src/pipex_utils.c
@@ -10,56 +10,60 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
+#include "pipex.h"	
 
-void    argc_error(int error_code)
+void	argc_error(int error_code)
 {
-    if (error_code == 0)
-        printf("pipex: error: not enough arguments.\nStandard use is ./pipex infile cmd cmd outfile\n");
-    if (error_code == 1)
-        printf("pipex: error: not enough arguments.\nUse with here_doc is ./pipex here_doc LIMITER cmd cmd1 file\n");
-    exit(1);
+	ft_putendl_fd("pipex: error: not enough arguments.", 2);
+	if (error_code == 0)
+		ft_putendl_fd("Standard use is ./pipex infile cmd cmd outfile", 2);
+	if (error_code == 1)
+	{
+		ft_putstr_fd("Use with here_doc is ", 2);
+		ft_putendl_fd("./pipex here_doc LIMITER cmd cmd1 file", 2);
+	}
+	exit(1);
 }
 
-/** Use perror to display the errno of the error and exit with the specified code.*/
-void    error_exit(int exit_code)
+//Use perror to display the errno and exit with the specified code.
+void	error_exit(int exit_code)
 {
-    perror("pipex: an error occured.");
-    exit(exit_code);
+	perror("pipex: an error occured.");
+	exit(exit_code);
 }
 
 /** Get the env variable from envp specified by str. */
-char    *ft_getenv(char **env, const char *str)
+char	*ft_getenv(char **env, const char *str)
 {
-    int     i;
-    char    *tmp;
+	int		i;
+	char	*tmp;
 
-    tmp = 0;
-    i = 0;
-    while (env && env[i])
-    {
-        if (!ft_strncmp(env[i], str, ft_strlen(str)))
-            tmp = env[i] + ft_strlen(str);
-        i++;
-    }
-    return (tmp);
+	tmp = 0;
+	i = 0;
+	while (env && env[i])
+	{
+		if (!ft_strncmp(env[i], str, ft_strlen(str)))
+			tmp = env[i] + ft_strlen(str);
+		i++;
+	}
+	return (tmp);
 }
 
-int    open_fd(int mode, char *filename)
+int	open_fd(int mode, char *filename)
 {
-    int fd;
+	int	fd;
 
-    if (mode == 0)
-        fd = open(filename, O_RDONLY, 0664);
-    if (mode == 1)
-        fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-    if (fd == -1)
-    {
-        ft_putstr_fd("pipex: ", 2);
-        ft_putstr_fd(filename, 2);
-        ft_putstr_fd(": ", 2);
-        perror("");
-        return(fd);
-    }
-    return (fd);
+	if (mode == 0)
+		fd = open(filename, O_RDONLY, 0664);
+	if (mode == 1)
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd == -1)
+	{
+		ft_putstr_fd("pipex: ", 2);
+		ft_putstr_fd(filename, 2);
+		ft_putstr_fd(": ", 2);
+		perror("");
+		return (fd);
+	}
+	return (fd);
 }
